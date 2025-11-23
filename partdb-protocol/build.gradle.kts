1bd83665afdc7215bec5b249a30d4816ced4f2fd
Taylor Brady <philiptaylorbrady@gmail.com>
2025-11-23 13:59:50 -0600

    Add client-server protocol module
    Define KVService protocol with support for basic operations, batching, consistency levels, and CAS. Upgrade gRPC to 1.75.0 and Protobuf to 4.33.1 across all protocol modules.

--- partdb-protocol/build.gradle.kts
+++ partdb-protocol/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
 }
 
 dependencies {
-    val grpcVersion = "1.68.1"
-    val protobufVersion = "4.29.1"
+    val grpcVersion = "1.75.0"
+    val protobufVersion = "4.33.1"
 
     implementation(project(":partdb-common"))
 
@@ -17,11 +17,11 @@ dependencies {
 
 protobuf {
     protoc {
-        artifact = "com.google.protobuf:protoc:4.29.1"
+        artifact = "com.google.protobuf:protoc:4.33.1"
     }
     plugins {
         create("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:1.68.1"
+            artifact = "io.grpc:protoc-gen-grpc-java:1.75.0"
         }
     }
     generateProtoTasks {
--- partdb-raft/build.gradle.kts
+++ partdb-raft/build.gradle.kts
@@ -5,25 +5,23 @@ plugins {
 dependencies {
     implementation(project(":partdb-common"))
 
-    val grpcVersion = "1.60.0"
-    val protobufVersion = "3.25.1"
+    val grpcVersion = "1.75.0"
+    val protobufVersion = "4.33.1"
 
     implementation("io.grpc:grpc-netty:$grpcVersion")
     implementation("io.grpc:grpc-protobuf:$grpcVersion")
     implementation("io.grpc:grpc-stub:$grpcVersion")
 
     implementation("com.google.protobuf:protobuf-java:$protobufVersion")
-
-    compileOnly("javax.annotation:javax.annotation-api:1.3.2")
 }
 
 protobuf {
     protoc {
-        artifact = "com.google.protobuf:protoc:3.25.1"
+        artifact = "com.google.protobuf:protoc:4.33.1"
     }
     plugins {
         create("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:1.60.0"
+            artifact = "io.grpc:protoc-gen-grpc-java:1.75.0"
         }
     }
     generateProtoTasks {
